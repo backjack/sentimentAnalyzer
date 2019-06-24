0e1cd1a63cbb032dc89333303479a3a16508f3f6
Neeraj <neeraj.s1988@gmail.com>
2019-06-25 01:31:15 +0530

    initial commit

---
+++ src/test/java/com/nts/sentimentAnalyzer/SentimentAnalyzerTest.java
@@ -0,0 +1,48 @@
+package com.nts.sentimentAnalyzer;
+
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+import org.ejml.simple.SimpleMatrix;
+import org.junit.Test;
+import java.util.Properties;
+
+public class SentimentAnalyzerTest {
+
+    @Test
+    public void test() {
+
+        String text = "I am sorry for my behaviour";
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        if (text != null && text.length() > 0) {
+
+            // run all Annotators on the text
+            Annotation annotation = pipeline.process(text);
+
+            for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+                // this is the parse tree of the current sentence
+                Tree tree = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+                SimpleMatrix sm = RNNCoreAnnotations.getPredictions(tree);
+                String sentimentType = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+
+                System.out.println("setVeryPositive"+(double)Math.round(sm.get(4) * 100d));
+                System.out.println("setPositive"+(double)Math.round(sm.get(3) * 100d));
+                        System.out.println("setNeutral"+(double)Math.round(sm.get(2) * 100d));
+                                System.out.println("setNegative"+(double)Math.round(sm.get(1) * 100d));
+                                        System.out.println("setVeryNegative"+(double)Math.round(sm.get(0) * 100d));
+                System.out.println(RNNCoreAnnotations.getPredictedClass(tree));
+                System.out.println(sentimentType);
+            }
+
+        }
+
+    }
+}
